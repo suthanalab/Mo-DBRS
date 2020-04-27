0d969f0cea5ab6c51ea5391714548e0dd4649f25
Uros Topalovic <urostop@protonmail.com>
2020-04-27 13:59:51 -0700

    C fix

--- Mo-DBRS/RaspberryPi/C/main.c
+++ Mo-DBRS/RaspberryPi/C/main.c
@@ -1,20 +1,75 @@
 #include "rns_server.h"
+#include <unistd.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 int main()
 {
+
     Parameters param;
-    param.ip_address = "192.168.0.5";
-    param.port = 8080;
-    param.dev_port = "/dev/ttyACM0";
-    param.rns_device = 320;
 
-    server_open(param);
+    FILE * fp;
+    char * line = NULL;
+    size_t len = 0;
+    ssize_t read;
 
-    while(1)
+    fp = fopen("./rns.conf", "r");
+    if (fp == NULL)
     {
-
+	    printf("Error. Configuration file does not exist.");
+	    return -1;
     }
 
-    server_close();
+    	char sep[] = " \n";
+	char* name;
+	char* value;
+
+	printf("\n\nLoaded Parameters:\n\n");
+
+    while ((read = getline(&line, &len, fp)) != -1) {
+	if(line[0] != '#')
+	{
+		name = strtok( line, sep );
+		if(!strcmp(name, "IP"))
+		{
+			value = strtok( NULL, sep );
+			printf("\tIP:\t%s\n", value );
+			param.ip_address = (char*)malloc(sizeof(char) * (strlen(value) + 1));
+			strcpy(param.ip_address, value);
+		}
+		else if(!strcmp(name, "PORT"))
+		{
+			value = strtok( NULL, sep );
+			printf( "\tPORT:\t%s\n", value );
+			param.port = atoi(value);
+		}
+		else if(!strcmp(name, "SERIAL"))
+		{
+			value = strtok( NULL, sep );
+			printf( "\tSERIAL:\t%s\n", value);
+			param.dev_port = (char*)malloc(sizeof(char) * (strlen(value) + 1));
+			strcpy(param.dev_port, value);
+		}
+		else if(!strcmp(name, "DEVICE"))
+		{
+			value = strtok( NULL, sep );
+			printf( "\tDEVICE:\t%s\n", value );
+			param.rns_device = atoi(value);
+		}
+
+	}
+    }
+
+    fclose(fp);
+    if (line)
+        free(line);
+
+
+    server_open(param);
+    free(param.ip_address);
+    free(param.dev_port);
+
+    server_join();
     return 0;
 }
