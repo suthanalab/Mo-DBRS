64a369d9ecb5708aec61d94cb450c229104c2786
Uros Topalovic <urostopalovic@ucla.edu>
2020-11-30 18:56:14 -0800

    Add files via upload

---
+++ Processing/iEEG/CommandDetection/epi_detection.m
@@ -0,0 +1,21 @@
+function [epi_idx] = epi_detection(data_batch, Fs)
+
+    data_zs = zscore(data_batch);
+    data_std = std(data_zs);
+    threshold_env = 5 * data_std;
+    threshold_filt = 6 * data_std;
+
+    [yupper, ] = envelope(data_zs);
+    env = yupper;
+    env_idx = env > threshold_env;
+    
+    bpFilt = designfilt('bandpassfir','FilterOrder',20, ...
+         'CutoffFrequency1',25,'CutoffFrequency2',80, ...
+         'SampleRate', Fs);
+     
+    data_filt = filtfilt(bpFilt, data_zs);
+    env_filt = abs(data_filt) > threshold_filt;
+    
+    epi_idx = env_idx | env_filt;
+end
+
