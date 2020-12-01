cf92797888707fb2c2989b74f8fb5da4f9722ecd
Uros Topalovic <urostop@protonmail.com>
2020-11-30 19:06:58 -0800

    Update epi_detection.m

--- Processing/iEEG/CommandDetection/epi_detection.m
+++ Processing/iEEG/CommandDetection/epi_detection.m
@@ -1,3 +1,5 @@
+%Simple first-order detection of epileptic activity
+%Returns indices when values are higher than set thresholds, we discarded batches that had any detection from further analysis.
 function [epi_idx] = epi_detection(data_batch, Fs)
 
     data_zs = zscore(data_batch);
@@ -17,5 +19,6 @@ function [epi_idx] = epi_detection(data_batch, Fs)
     env_filt = abs(data_filt) > threshold_filt;
     
     epi_idx = env_idx | env_filt;
+
 end
 
